09edbfa4e5541f331bd09d8102781859aa8ff618
Maciej Konieczny <hello@narf.pl>
2010-04-20 15:32:21 +0200

    refactored runner

--- script_runner.py
+++ script_runner.py
@@ -20,30 +20,27 @@ from itertools import imap
 from os.path import dirname, isfile, join, realpath
 from pipes import quote
 from subprocess import call
-from sys import argv
+from sys import argv, stderr
 
 
 __author__ = 'Maciej Konieczny <hello@narf.pl>'
 
 
-def usage():
-    print(__doc__)
-
-
 def _main():
     if len(argv) < 2:
-        usage()
+        print >> stderr, __doc__[1:-1]
+        exit(1)
+
+    loop_name = argv[1] + '.py'
+    parameters = ' '.join(imap(quote, argv[2:]))
+
+    loops_dir = join(dirname(__file__), '..')
+    loop_path = realpath(join(loops_dir, loop_name))
+
+    if isfile(loop_path):
+        call('python {0} {1}'.format(loop_path, parameters), shell=True)
     else:
-        loop_name = argv[1] + '.py'
-        parameters = ' '.join(imap(quote, argv[2:]))
-
-        loops_dir = join(dirname(__file__), '..')
-        loop_path = realpath(join(loops_dir, loop_name))
-
-        if isfile(loop_path):
-            call('python {0} {1}'.format(loop_path, parameters), shell=True)
-        else:
-            print('File not found: ' + loop_path)
+        print('File not found: ' + loop_path)
 
 if __name__ == '__main__':
     _main()
